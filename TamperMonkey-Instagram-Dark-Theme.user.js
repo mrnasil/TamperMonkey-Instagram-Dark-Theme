f8622a76c3056813de30547c684e7672a1fa79d6
Lei <leidizayn@gmail.com>
2022-01-23 02:34:33 +0300

    Minified

--- TamperMonkey-Instagram-Dark-Theme.user.js
+++ TamperMonkey-Instagram-Dark-Theme.user.js
@@ -2,7 +2,7 @@
 // @name	TamperMonkey Instagram Dark Theme
 // @namespace	https://github.com/mrnasil/TamperMonkey-Instagram-Dark-Theme
 // @description	TamperMonkey Instagram Dark Theme is an userscript which aims to change the theme of the Instagram.com website, and make it a dark theme when the system is in dark mode.
-// @version	1.0.1
+// @version	1.0.2
 // @encoding utf-8
 // @license	MIT
 // @updateURL	https://raw.githubusercontent.com/mrnasil/TamperMonkey-Instagram-Dark-Theme/main/TamperMonkey-Instagram-Dark-Theme.user.js
@@ -20,30 +20,4 @@
 =======================================================
 Script
 ======================================================*/
-
-(function () {
-	"use strict";
-	const current = window.matchMedia("(prefers-color-scheme: dark)").matches;
-	function setColorScheme(st) {
-		const qs = new URLSearchParams(document.location.search);
-		if (
-			(st && qs.get("theme") != "dark") ||
-			(!st && qs.get("theme") == "dark") ||
-			current != st
-		) {
-			console.log("change theme");
-			if (st && qs.get("theme") != "dark") qs.set("theme", "dark");
-			else qs.delete("theme");
-			const dl = document.location;
-			document.location = `${dl.origin}${dl.pathname}${
-				qs.toString().length > 0 ? "?" : ""
-			}${qs.toString()}`;
-		}
-	}
-	setColorScheme(current);
-	window
-		.matchMedia("(prefers-color-scheme: dark)")
-		.addEventListener("change", (e) => {
-			setColorScheme(e.matches);
-		});
-})();
+!function(){"use strict";const current=window.matchMedia("(prefers-color-scheme: dark)").matches;function setColorScheme(st){const qs=new URLSearchParams(document.location.search);if(st&&"dark"!=qs.get("theme")||!st&&"dark"==qs.get("theme")||current!=st){console.log("change theme"),st&&"dark"!=qs.get("theme")?qs.set("theme","dark"):qs.delete("theme");const dl=document.location;document.location=`${dl.origin}${dl.pathname}${qs.toString().length>0?"?":""}${qs.toString()}`}}setColorScheme(current),window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change",e=>{setColorScheme(e.matches)})}();
